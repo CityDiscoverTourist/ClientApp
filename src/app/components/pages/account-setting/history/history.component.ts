eff664b368412f300521a88ede85a55fccaf1b3a
ncc304 <nguyenchicuong304@gmail.com>
2022-07-10 22:48:36 +0700

    sidebar

--- src/app/components/pages/account-setting/history/history.component.ts
+++ src/app/components/pages/account-setting/history/history.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Payment } from "src/app/models/payment.model";
 
 
 @Component({
@@ -6,11 +7,46 @@ import { Component, OnInit } from "@angular/core";
     templateUrl: "./history.component.html",
     styleUrls: ["./history.component.scss"],
 })
-export class HistoryComponent implements OnInit {
 
+export class HistoryComponent implements OnInit {
+    public payment : Payment;
 
     constructor() {}
 
     ngOnInit(): void {
     }
+
+    countries: Country[] = [
+        {
+          name: 'Russia',
+          flag: 'f/f3/Flag_of_Russia.svg',
+          area: 17075200,
+          population: 146989754
+        },
+        {
+          name: 'Canada',
+          flag: 'c/cf/Flag_of_Canada.svg',
+          area: 9976140,
+          population: 36624199
+        },
+        {
+          name: 'United States',
+          flag: 'a/a4/Flag_of_the_United_States.svg',
+          area: 9629091,
+          population: 324459463
+        },
+        {
+          name: 'China',
+          flag: 'f/fa/Flag_of_the_People%27s_Republic_of_China.svg',
+          area: 9596960,
+          population: 1409517397
+        }
+      ];
 }
+
+interface Country {
+    name: string;
+    flag: string;
+    area: number;
+    population: number;
+  }
